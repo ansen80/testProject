f2ed08e5d1dcdd822d047ae8a1c9431905dbf399
Andrey Sender <ansen80@inbox.ru>
2024-12-10 09:15:06 +0300

    Selenide Pobeda

--- src/test/java/PobedaSelenide/Action.java
+++ src/test/java/PobedaSelenide/Action.java
@@ -15,50 +15,49 @@ public class Action {
     private SelenideElement searchButton = $(By.xpath("//button[@type='submit']"));
     private SelenideElement errorMessage = $(By.xpath("//div[@ng-if=\"vm.errorMessage\"]"));
 
-    // Заголовок страницы
+    //Заголовок страницы
     public boolean isPageTitleCorrect(String expectedTitle) {
         return title().equals(expectedTitle);
     }
 
-    // Наличие логотипа
+    //Наличие логотипа
     public boolean isLogoDisplayed() {
         return logo.isDisplayed();
     }
 
-    // Кликаем на Управление бронированием
+    //Клик на Управление бронированием
     public void clickManageBooking() {
         manageBookingLink.click();
     }
 
-    // Проверяем, что кнопки видны на странице
+    //Копки видны на странице
     public boolean areBookingFieldsVisible() {
         return lastNameField.isDisplayed() && bookingNumberField.isDisplayed() && searchButton.isDisplayed();
     }
 
-    // Вводим данные и нажимаем кнопку поиск
+    //Ввод данных и клик поиск
     public void searchBooking(String lastName, String bookingNumber) {
         lastNameField.setValue(lastName);
         bookingNumberField.setValue(bookingNumber);
         searchButton.click();
     }
 
-    // Ждем новое окно
+    //Ждем новое окно
     public void waitForWindow() {
         switchTo().window(1);
     }
 
-    // Переход на новое окно
+    //Новое окно
     public void switchToNewWindow() {
-        // Переключаемся на новое окно (индекс 1, так как 0 — это основное окно)
-        switchTo().window(1);
+        switchTo().window(1); //Переключаемся на новое окно
     }
 
     public boolean isErrorMessageDisplayed() {
         try {
-            errorMessage.shouldBe(visible); // Ожидание появления ошибки
+            errorMessage.shouldBe(visible);
             String actualErrorMessage = errorMessage.getText();
             if (!actualErrorMessage.toLowerCase().contains("заказ с указанными параметрами не найден")) {
-                throw new AssertionError("Ошибка: " + actualErrorMessage); // Выбрасываем исключение, если ошибка не совпадает
+                throw new AssertionError("Ошибка: " + actualErrorMessage);
             }
             return true;
         } catch (Exception e) {
--- src/test/java/PobedaSelenide/PobedaTest.java
+++ src/test/java/PobedaSelenide/PobedaTest.java
@@ -14,37 +14,25 @@ public class PobedaTest {
     @Before
     public void setup() {
         WebDriverManager.chromedriver().setup();
-        Configuration.timeout = 10000; // Тайм-ауты для поиска элементов
+        Configuration.timeout = 10000;
         Selenide.open("https://www.pobeda.aero");
         action = new Action();
     }
 
     @Test
     public void testManageBooking() {
-        // Используем метод isPageTitleCorrect для проверки заголовка страницы
+
         String expectedTitle = "Авиакомпания «Победа» - купить авиабилеты онлайн, дешёвые билеты на самолёт, прямые и трансферные рейсы с пересадками";
         Assert.assertTrue("Текст заголовка страницы некорректен", action.isPageTitleCorrect(expectedTitle));
 
-        // Использование метода из класса Action для проверки логотипа
         Assert.assertTrue("Логотип Победы не отображается", action.isLogoDisplayed());
 
-        // Клик по кнопке "Управление бронированием"
         action.clickManageBooking();
-
-        // Проверка видимости полей для ввода
         Assert.assertTrue("Не отображаются поля для ввода на странице управления бронированием",
                 action.areBookingFieldsVisible());
-
-        // Ввод данных и нажатие кнопки "Поиск"
         action.searchBooking("Qwerty", "XXXXXX");
-
-        // Ожидание появления нового окна
         action.waitForWindow();
-
-        // Переключение на новое окно
         action.switchToNewWindow();
-
-        // Проверка наличия ошибки, используя метод isErrorMessageDisplayed
         Assert.assertTrue("Ошибка не отображается", action.isErrorMessageDisplayed());
     }
 }
