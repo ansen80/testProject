2d1dd96533533e5e2aefd0105026ad4aac18c713
Andrey Sender <ansen80@inbox.ru>
2024-12-15 18:23:01 +0300

    Добавил Allure отчет

--- src/test/java/SelenidePobeda/Action.java
+++ src/test/java/SelenidePobeda/Action.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
+import io.qameta.allure.Step;
+
 public class Action {
 
     private SelenideElement logo = $(By.cssSelector("img[src*='logo-rus-white']"));
@@ -15,42 +17,43 @@ public class Action {
     private SelenideElement errorMessage = $(By.xpath("//div[@ng-if=\"vm.errorMessage\"]"));
 
     //Заголовок страницы
+    @Step("Проверка заголовка страницы")
     public boolean isPageTitleCorrect(String expectedTitle) {
         return title().equals(expectedTitle);
     }
-
     //Наличие логотипа
+    @Step("Проверка отображения логотипа")
     public boolean isLogoDisplayed() {
         return logo.isDisplayed();
     }
-
     //Клик на Управление бронированием
+    @Step("Клик на кнопку 'Управление бронированием'")
     public void clickManageBooking() {
         manageBookingLink.click();
     }
-
-    //Копки видны на странице
+    //Кнопки видны на странице
+    @Step("Проверка видимости полей для ввода бронирования")
     public boolean areBookingFieldsVisible() {
         return lastNameField.isDisplayed() && bookingNumberField.isDisplayed() && searchButton.isDisplayed();
     }
-
-    //Ввод данных и клик поиск
+    //Ввод данных и клик по кнопке поиска
+    @Step("Ввод фамилии клиента и номера бронирования: {0}, {1}")
     public void searchBooking(String lastName, String bookingNumber) {
         lastNameField.setValue(lastName);
         bookingNumberField.setValue(bookingNumber);
         searchButton.click();
     }
-
     //Ждем новое окно
+    @Step("Ожидание нового окна")
     public void waitForWindow() {
         switchTo().window(1);
     }
-
     //Новое окно
+    @Step("Переключение на новое окно")
     public void switchToNewWindow() {
         switchTo().window(1); //Переключаемся на новое окно
     }
-
+    @Step("Проверка отображения ошибки")
     public boolean isErrorMessageDisplayed() {
         try {
             errorMessage.shouldBe(visible);
--- src/test/java/SelenidePobeda/PobedaTest.java
+++ src/test/java/SelenidePobeda/PobedaTest.java
@@ -7,6 +7,13 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Epic;
+
+@Epic("Автотесты для сайта Pobeda")
+@Feature("Проверка управления бронированием")
+
 public class PobedaTest {
 
     private Action action;
@@ -20,6 +27,8 @@ public class PobedaTest {
     }
 
     @Test
+    @Description("Проверка функциональности управления бронированием с ожидаемым положительным результатом")
+    @Feature("Проверка управления бронированием")
     public void testManageBooking() {
         String expectedTitle = "Авиакомпания «Победа» - купить авиабилеты онлайн, дешёвые билеты на самолёт, прямые и трансферные рейсы с пересадками";
         Assert.assertTrue("Текст заголовка страницы некорректен", action.isPageTitleCorrect(expectedTitle));
@@ -28,6 +37,22 @@ public class PobedaTest {
         Assert.assertTrue("Не отображаются поля для ввода на странице управления бронированием",
                 action.areBookingFieldsVisible());
         action.searchBooking("Qwerty", "XXXXXX");
+        action.waitForWindow();
+        action.switchToNewWindow();
+        Assert.assertTrue("Ошибка не отображается", action.isErrorMessageDisplayed());
+    }
+
+    @Test
+    @Description("Проверка управления бронированием с неправильными данными для негативного теста")
+    @Feature("Проверка управления бронированием")
+    public void testManageBookingFail() {
+        String expectedTitle = "Авиакомпания «Победа» - купить авиабилеты онлайн, дешёвые билеты на самолёт, прямые и трансферные рейсы с пересадками";
+        Assert.assertTrue("Текст заголовка страницы некорректен", action.isPageTitleCorrect(expectedTitle));
+        Assert.assertTrue("Логотип Победы не отображается", action.isLogoDisplayed());
+        action.clickManageBooking();
+        Assert.assertTrue("Не отображаются поля для ввода на странице управления бронированием",
+                action.areBookingFieldsVisible());
+        action.searchBooking("Invalid", "00000");  //Логин и пароль не тот.
         action.waitForWindow();
         action.switchToNewWindow();
         Assert.assertTrue("Ошибка не отображается", action.isErrorMessageDisplayed());
