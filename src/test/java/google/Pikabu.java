f13f94baed87afe84c934f21c4d124b82d8507fc
Andrey Sender <ansen80@inbox.ru>
2024-12-22 09:08:12 +0300

    Добавил Allure отчет

--- src/test/java/google/Pikabu.java
+++ src/test/java/google/Pikabu.java
@@ -4,9 +4,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Pikabu {
@@ -27,25 +25,32 @@ public class Pikabu {
 
         Assert.assertEquals(driver.getCurrentUrl(), "https://pikabu.ru/");
 
-        Assert.assertEquals(driver.getTitle(),"Горячее – самые интересные и обсуждаемые посты | Пикабу");
+        Assert.assertEquals(driver.getTitle(), "Горячее – самые интересные и обсуждаемые посты | Пикабу");
 
-        driver.findElement(By.cssSelector("[class=\"pkb-normal-btn header-right-menu__login-button\"]")).click();
+        WebElement loginButton = driver.findElement(By.xpath("//button[contains(text(), 'Войти')]"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", loginButton);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", loginButton);
 
-        driver.findElement(By.cssSelector("[class=\"auth-modal\"]")).isDisplayed();
-        driver.findElement(By.cssSelector("[name=\"username\"]")).isDisplayed();
-        driver.findElement(By.cssSelector("[name=\"password\"]")).isDisplayed();
+
+        driver.findElement(By.cssSelector("div[class=\"auth-modal\"]")).isDisplayed();
+        driver.findElement(By.xpath("(//input[@class=\"input__input\"])[11]")).isDisplayed();
+        driver.findElement(By.xpath("(//input[@class=\"input__input\"])[12]")).isDisplayed();
         driver.findElement(By.cssSelector("button.button_success.button_width_100[type=\"submit\"]")).isDisplayed();
 
-        driver.findElement(By.cssSelector("[name=\"username\"]")).sendKeys("Qwerty");
-        driver.findElement(By.cssSelector("[name=\"password\"]")).sendKeys("Qwerty");
+        driver.findElement(By.xpath("(//input[@class=\"input__input\"])[11]")).sendKeys("Qwerty");
+        driver.findElement(By.xpath("(//input[@class=\"input__input\"])[12]")).sendKeys("Qwerty");
         driver.findElement(By.cssSelector("button.button_success.button_width_100[type=\"submit\"]")).sendKeys(Keys.ENTER);
 
-        Assert.assertTrue(driver.findElement(By.cssSelector("//span[class=\"auth__error auth__error_top\"]")).isDisplayed());
-        Assert.assertEquals(driver.getTitle(), "Ошибка. Вы ввели неверные данные авторизации");
+
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@class=\"auth__notice\"]")).isDisplayed());
+        WebElement element = driver.findElement(By.xpath("//div[@class='auth__notice']"));
+        String actualText = element.getText();
+        Assert.assertTrue("Текст не совпадает", actualText.contains("Необходимо войти или зарегистрироваться"));
 
     }
+
     @After
-    public void tearDown( ){
+    public void tearDown() {
         driver.quit();
     }
 }
